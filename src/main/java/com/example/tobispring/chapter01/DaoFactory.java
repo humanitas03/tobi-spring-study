8cfaf0f2de69d64d93817670b2871213b077255b
humanitas03 <dlswp113@gmail.com>
2021-05-29 14:05:58 +0900

    ch 1.4.2 DaoFactory의 ConnectionMaker분리

--- src/main/java/com/example/tobispring/chapter01/DaoFactory.java
+++ src/main/java/com/example/tobispring/chapter01/DaoFactory.java
@@ -8,8 +8,16 @@ public class DaoFactory {
 
   public UserDao userDao(){
     //팩토리의 메소드는 UserDao 타입의 오브젝트를 어떻게 만들고, 어떻게 준비시킬지 결정.
-    ConnectionMaker connectionMaker = new MysqlConnectionMaker();
-    UserDao userDao = new UserDao(connectionMaker);
-    return userDao;
+    /**
+     *  구현해야될 DAO가 많아지는 경우 생성 메서드의 중복이 발생하므로,
+     *  connectionMaker를 분리해서 중복을 제거하도록 한다.
+     */
+
+    return new UserDao(connectionMaker());
+  }
+
+
+  public ConnectionMaker connectionMaker(){
+    return new MysqlConnectionMaker();
   }
 }
