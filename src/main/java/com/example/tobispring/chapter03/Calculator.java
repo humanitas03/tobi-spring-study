209efa4c091f78bed773878188278bd61925b220
humanitas03 <dlswp113@gmail.com>
2021-06-22 13:28:07 +0900

    ch.3.5.3.3 템플릿/콜백 재설계(LineCallBack)

--- src/main/java/com/example/tobispring/chapter03/Calculator.java
+++ src/main/java/com/example/tobispring/chapter03/Calculator.java
@@ -10,55 +10,44 @@ import java.io.IOException;
 import java.nio.Buffer;
 
 public class Calculator {
-
-  /*BufferedReaderCallback을 사용하는 템플릿 메소드*/
-  public Integer fileReadTemplate(String filepath, BufferedReaderCallBack callback) throws IOException{
+  /*Line 콜백을 사용하는 템플릿*/
+  public Integer lineReadTemplate(String filepath, LineCallBack lineCallBack, int initVal) throws IOException {
     BufferedReader br = null;
-    try{
+
+    try {
       br = new BufferedReader(new FileReader(filepath));
-      int ret = callback.doSomethingWithReader(br);
-      return ret;
-    }
-    catch(IOException e) {
-      System.out.println(e.getMessage());
-      throw e;
-    }
+      Integer res = initVal;
+      String line = null;
+      while ((line = br.readLine()) != null) {
+        res = lineCallBack.doSomethingWtihLine(line, res);
+      }
+      return res;
+    } catch(IOException e) {
+        System.out.println(e.getMessage());
+        throw e;
+      }
     finally {
-      if(br!=null) {
-        try{
-          br.close();
-        }catch(IOException e){
-          System.out.println(e.getMessage());
+        if(br!=null) {
+          try{
+            br.close();
+          }catch(IOException e){
+            System.out.println(e.getMessage());
+          }
         }
       }
-    }
   }
 
+  /** lineReadTemplate()을 사용하도록 수정한 calCum(), calcMultiply() 메소드 */
   /** 템플릿 콜백을 적용한 calcSum()메서드 */
   public Integer calcSum(String filepath) throws IOException {
     System.out.println("file path : "+ filepath);
-    BufferedReaderCallBack sumCallBack = br -> {
-      Integer sum = 0;
-      String line = null;
-      while((line=br.readLine())!=null) {
-        sum += Integer.valueOf(line);
-      }
-      return sum;
-    };
-    return fileReadTemplate(filepath, sumCallBack);
+    LineCallBack sumCallBack = (line, value) -> value + Integer.valueOf(line);
+    return lineReadTemplate(filepath, sumCallBack, 0);
   }
 
   /** 곱을 계산하는 콜백을 가진 caclMultiply() 메소드 */
   public Integer calcMultiply(String filePath) throws IOException {
-    BufferedReaderCallBack multiplyCallback = br -> {
-      Integer multiply = 1;
-      String line = null;
-      while((line=br.readLine())!=null) {
-        multiply *= Integer.valueOf(line);
-      }
-      return multiply;
-    };
-
-    return fileReadTemplate(filePath, multiplyCallback);
+   LineCallBack multiplyCallBack = (line, value) -> value * Integer.valueOf(line);
+   return lineReadTemplate(filePath, multiplyCallBack, 1);
   }
 }
---
+++ src/main/java/com/example/tobispring/chapter03/LineCallBack.java
@@ -0,0 +1,9 @@
+/**
+ * =============================================================== File name : LineCallBack.java
+ * Created by injeahwang on 2021-06-22 ===============================================================
+ */
+package com.example.tobispring.chapter03;
+
+public interface LineCallBack {
+  Integer doSomethingWtihLine(String line, Integer value);
+}
