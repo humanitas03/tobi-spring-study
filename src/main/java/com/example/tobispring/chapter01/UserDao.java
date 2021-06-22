14c56030d0e5aaed41f886eb0a39605f4c9c4cb4
humanitas03 <dlswp113@gmail.com>
2021-06-22 10:07:04 +0900

    ch.3.3.1 add를 전략으로 추가

--- src/main/java/com/example/tobispring/chapter01/UserDao.java
+++ src/main/java/com/example/tobispring/chapter01/UserDao.java
@@ -21,24 +21,11 @@ public class UserDao {
 
     /** DB 저장 결과
      * ch-1.3.1 독립된 SimpleConnectionMaker를 주입받아 커넥션 실행
+     * ch-3.3.1 user정보를 AddStatement에 전달.
      * */
     public void add(User user) throws SQLException {
-        Connection c = dataSource.getConnection();
-
-        // SQL을 담은 PreparedStatement를 가져온다.
-        PreparedStatement ps = c.prepareStatement(
-                "insert into users values(?,?,?)" //h2 Query
-//            "insert into users('id','name',password') value(?,?,?)" //Mysql Query
-        );
-
-        ps.setString(1, user.getId());
-        ps.setString(2,user.getName());
-        ps.setString(3, user.getPassword());
-
-        ps.executeUpdate();
-
-        ps.close();
-        c.close();
+        StatementStrategy st = new AddStatement(user);
+        jdbcContextWithStatementStrategy(st);
     }
 
     /** 조회 실행 결과
